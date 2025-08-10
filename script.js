c7448965de8dc49ac79d00afea885bc5e4b620c3
TheRohanDev <rohansonawane2649@gmail.com>
2025-08-10 15:04:59 +0530

    Go with you

--- script.js
+++ script.js
@@ -1,3 +1,76 @@
+// window.addEventListener("load", function() {
+//     const hash = window.location.hash;
+
+//     if (hash) {
+//         // Sabhi elements jinme id ho — aur div ya section tag ho — unko hide karo
+//         const allSections = document.querySelectorAll("div[id], section[id]");
+//         allSections.forEach((el) => {
+//             el.style.display = "none";
+//         });
+
+//         // URL ke hash se jo id mili hai usko show karo
+//         const target = document.querySelector(hash);
+//         if (target) {
+//             target.style.display = "block";
+//         }
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *******************************************important code for [Direct open website data ]*************************************************************************************
+
+
+
+
+// window.addEventListener("DOMContentLoaded", function() {
+//     const hash = window.location.hash || "#defaultsection";
+
+//     const allSections = document.querySelectorAll("div[id], section[id]");
+//     allSections.forEach((el) => {
+//         el.style.display = "none";
+//     });
+
+//     setTimeout(() => {
+//         const target = document.querySelector(hash);
+//         if (target) {
+//             target.style.display = "block";
+//             target.scrollIntoView({
+//                 behavior: "instant",
+//                 block: "start"
+//             });
+//         }
+//     }, 10);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// *******************************************important code for [ 5 sec animation then Main website data ] *************************************************************************************
+
+
+
+
+
 window.addEventListener("DOMContentLoaded", function() {
     const hash = window.location.hash || "#defaultsection";
 
@@ -26,5 +99,5 @@ window.addEventListener("DOMContentLoaded", function() {
                 block: "start"
             });
         }
-    }, 5000); // ⏳ 5 seconds loading
+    }, 3000); // ⏳ 5 seconds loading
 });
